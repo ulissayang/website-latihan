1b67da5b0267f83d16d978228e6859a99c20b2e9
ulissayang <ulissleksmart@gmail.com>
2023-03-31 16:52:50 +0900

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["index.html"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {},
+  },
+  screens: {
+    "2xl": "1320px",
   },
   plugins: [],
 };
