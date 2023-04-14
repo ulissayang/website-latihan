fcf17b1237909c75107f0c621c34fe27a64f3462
ulissayang <ulissleksmart@gmail.com>
2023-04-14 12:29:45 +0900

    darkmode1

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,80 @@ hamburger.addEventListener("click", function () {
     home.classList.add("hidden");
   }
 });
+
+// sub menu program study ketika di hovver
+const dropdownToggle = document.querySelector("#dropdown-toggle");
+const dropdownMenu = document.querySelector("#dropdown-menu");
+const dropdownContent = document.querySelector("#dropdown-content");
+
+dropdownToggle.addEventListener("mouseenter", () => {
+  if (window.innerWidth >= 768) {
+    dropdownMenu.style.display = "block";
+  }
+});
+
+dropdownContent.addEventListener("mouseleave", () => {
+  if (window.innerWidth >= 768) {
+    dropdownMenu.style.display = "none";
+  }
+});
+
+// toggle mode dark and white
+// const checkbox = document.querySelector("#toggle");
+// const html = document.querySelector("html");
+
+// checkbox.addEventListener("click", function () {
+//   checkbox.click ? html.classList.add("dark") : html.classList.remove("dark");
+//   if (checkbox.click) {
+//     localStorage.theme = "dark";
+//   } else {
+//     localStorage.theme = "light";
+//   }
+// });
+
+// // pindahkan posisi toggle sesuia mode
+// if (
+//   localStorage.theme === "dark" ||
+//   (!("theme" in localStorage) &&
+//     window.matchMedia("(prefers-color-scheme: dark)").matches)
+// ) {
+//   checkbox.checked = true;
+// } else {
+//   checkbox.checked = false;
+// }
+
+const checkbox = document.querySelector("#toggle");
+const html = document.querySelector("html");
+const darkIcon = document.querySelector("#toggle svg:first-child");
+const lightIcon = document.querySelector("#toggle svg:last-child");
+
+checkbox.addEventListener("click", function () {
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    darkIcon.classList.remove("hidden");
+    lightIcon.classList.add("hidden");
+    localStorage.theme = "light";
+  } else {
+    html.classList.add("dark");
+    darkIcon.classList.add("hidden");
+    lightIcon.classList.remove("hidden");
+    localStorage.theme = "dark";
+  }
+});
+
+// pindahkan posisi toggle sesuai mode
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  html.classList.add("dark");
+  darkIcon.classList.add("hidden");
+  lightIcon.classList.remove("hidden");
+  checkbox.checked = true;
+} else {
+  html.classList.remove("dark");
+  darkIcon.classList.remove("hidden");
+  lightIcon.classList.add("hidden");
+  checkbox.checked = false;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html"],
+  darkMode: "class",
   theme: {
-    screens: { tablet: "561px", laptop: "769px" },
+    screens: { tablet: "561px", laptop: "769px", larg: "1200px" },
     container: {
       center: true,
       padding: "16px",
